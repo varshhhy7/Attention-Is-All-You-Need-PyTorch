5d277599307bfb6caf741490b3437fda089a2a66
Varshith <manivarshithpc@gmail.com>
2025-09-20 23:41:02 +0530

    feed-forward NN

--- model.py
+++ model.py
@@ -45,4 +45,14 @@ class LayerNormalization(nn.Module):
         mean = x.mean(dim=-1, keepdim=True)
         std = x.std(dim=-1, keepdim=True)
         return self.alpha * (x - mean) / (std + self.eps) + self.bias
+    
+class FeedForward(nn.Module):
+    def __init__(self, d_model: int, d_ff: int, dropout: float)->None:
+        super().__init__()
+        self.linear1 = nn.Linear(d_model, d_ff)
+        self.dropout = nn.Dropout(dropout)
+        self.linear2=nn.Linear(d_ff , d_model)
+
+    def forward(self,x):
+        return self.linear2_2(self.dropout(torch.relu(self.linear1(x))))
 
