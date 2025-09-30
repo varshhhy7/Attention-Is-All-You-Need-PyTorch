5e02d305b17d3f87d5dd2ffebdfdf7e7cfc16165
Varshith <manivarshithpc@gmail.com>
2025-09-30 20:25:13 +0530

    UPDATED MULTIHADED ATTENTION

--- model.py
+++ model.py
@@ -71,8 +71,37 @@ class MultiHeadAttention(nn.Module):
         self.w_0=nn.Linear(d_model , d_model) #w0
         self.dropout = nn.Dropout(dropout)
 
+    @staticmethod
+    def attention(query, key , value , mask , droput : nn.Dropout):
+        d_k=query.shape[-1]
+         
+
+         #
+        attention_scores = (query @ key.transpose(-2 , -1))/math.sqrt(d_k)
+        if mask is not None:
+            attention_scores.masked_fill(mask==0 , -1e9)
+        attention_scores= attention_scores.softmax(dim=1) 
+
+        if dropout is not None:
+            attention_scores = droput(attention_scores)
+        
+        return (attention_scores @ value) , attention_scores
     def forward(self , q, k, v, mask):  
         query = self.w_q(q) 
         key = self.w_k(k)
         value = self.w_v(v) 
+
+        query= query.view(query.shape[0],query.shape[1],self.h , self.d_k).transpose(1,2)
+        key= key.view(key.shape[0],key.shape[1],self.h, self.d_k).transpose(1,2)
+        value=value.view(value.shape[0],value.shape[1],self.h,self.d_k).transpose(1,2)
+
+
+        x, self.attention_scores=MultiHeadAttention.attention(query,  key, value , mask , self.dropout)
+
+   #(batch_h,seq_len , h , d_k) -> (batch_h , h , seq_len , d_k) -> (batch_h , seq_len , h , d_k) -> (batch_h , seq_len , d_model)
+        x=x.transpose(1,2).contiguous().view(x.shape[0],-1, self.h*self.d_k)
+#batch,seq_len , d_model --> (batch,seq_len , d_model)
+        return self.w_0(x)
+
+
 
